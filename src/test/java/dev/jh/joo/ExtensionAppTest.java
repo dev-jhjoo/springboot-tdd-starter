8484e8a2cd7b764b845272d4dc3c6e66545675eb
jh.joo <dev.jh.joo@gmail.com>
2021-01-13 22:19:22 +0900

    Study JUnit5 1. 테스트 실행순서 조작, MethodOrderApp 2. 테스트 인스턴스 공유, TestInstance 3. 테스트 설정, PropertiesApp 4. JUnit5 확장기능, ExtenstionApp

---
+++ src/test/java/dev/jh/joo/ExtensionAppTest.java
@@ -0,0 +1,48 @@
+package dev.jh.joo;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+//JUnit5 확장모델
+class ExtensionAppTest implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+
+    private static final long THRESHOLD = 1000L;
+
+    @Override
+    public void beforeTestExecution(ExtensionContext context) throws Exception {
+        ExtensionContext.Store store = getStore(context);
+        store.put("START_TIME", System.currentTimeMillis());
+    }
+
+    @Override
+    public void afterTestExecution(ExtensionContext context) throws Exception {
+        Method requiredTestMethod = context.getRequiredTestMethod();
+        CustomAnnotationTest annotation = requiredTestMethod.getAnnotation(CustomAnnotationTest.class);
+
+        String methodName = context.getRequiredTestMethod().getName();
+        ExtensionContext.Store store = getStore(context);
+
+        long start_time = store.remove("START_TIME", long.class);
+        long duration = System.currentTimeMillis() - start_time;
+
+        // 1s 보다 긴 테스트에 경우 해당 msg 출력
+        if(duration > THRESHOLD && annotation == null) {
+            System.out.printf("[WRAN]Please consider mark method [%s] with @CustomAnnotationTest.\n", methodName);
+        }
+    }
+
+    private ExtensionContext.Store getStore(ExtensionContext context){
+        String className = context.getRequiredTestClass().getName();
+        String methodName = context.getRequiredTestMethod().getName();
+        return context.getStore(ExtensionContext.Namespace.create(
+                className, methodName
+        ));
+    }
+}
