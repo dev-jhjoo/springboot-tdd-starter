3c727e9643fb480baadf6a4e5379f298e3767401
jh.joo <dev.jh.joo@gmail.com>
2021-01-24 00:03:50 +0900

    Study test-containers * test-containers에서 docker 설정 방법 * @Slf4j * test에서 실제 Spring/ApplicationContext 데이터 확인

--- src/test/java/dev/jh/joo/testcontainers/ClassServiceTest.java
+++ src/test/java/dev/jh/joo/testcontainers/ClassServiceTest.java
@@ -1,23 +1,28 @@
 package dev.jh.joo.testcontainers;
 
 
-import org.junit.jupiter.api.AfterAll;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-import java.util.Optional;
 
-import static org.mockito.BDDMockito.given;
 
 /*****************************************************************
  * spring은 기본적으로 각각의 DB의 정책을 따라가기때문에 ex.isolation level...
@@ -36,10 +41,19 @@ import static org.mockito.BDDMockito.given;
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @Testcontainers
+@Slf4j // 10.
+@ContextConfiguration(initializers = ClassServiceTest.ContainerPropertyInitializer.class)
 public class ClassServiceTest {
+
+    // 10.이렇게 생성하는것 대신에 @Slf4j 를 사용할 수 있다.
+    Logger LOGGER = LoggerFactory.getLogger(ClassServiceTest.class);
+
 
     @Autowired
     ClassesRepository classesRepository;
+
+    @Autowired
+    Environment environment;
 
 //    @Container
 //    static PostgreSQLContainer postgreSQLContainer =
@@ -52,14 +66,36 @@ public class ClassServiceTest {
     // local 저장소를 먼저 검색 후 없으면 download 후 사용
     @Container
     static GenericContainer genericContainer = new GenericContainer("postgres")
-            .withEnv("POSTGRES_DB","gc-test");
+            .withExposedPorts(5432) // local port 지정
+//            .waitingFor(Wait.forListeningPort())
+//            .waitingFor(Wait.forHttp("/test/check")) // 이런식으로 원하는 end-point에 요청이 오는지, 요청이 올때까지 기다리는 그런 기능
+            .withEnv("POSTGRES_HOST_AUTH_METHOD" ,"trust")
+//            .withEnv("POSTGRES_PASSWORD", "jhjoo")
+//            .withEnv("POSTGRES_DB", "tc-test")
+    ;
+
+    @BeforeAll
+    static void beforeAll(){
+        // 10.log 는 @Slf4j annotation에서 기본으로 만들어줌
+        Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(log);
+        genericContainer.followOutput(logConsumer); // container 안에 생성되는 log도 확인 가능
+    }
 
 
     // Containers를 static으로 공유하고 해당 자원을 각 테스트 실행시 마다 deleteAll하면 좀 더 효율적으로 테스트 가능
     @BeforeEach
     void beforeEach(){
-        System.out.println("Before each");
+        log.info("followOutput = Before each =================");
+        // local 5432 port에 mapping된 docker port 확인
+//        System.out.println("getMappedPort=" + genericContainer.getMappedPort(15432));
+        System.out.println("getLogs=" + genericContainer.getLogs()); // container log 확인
+        System.out.println("============");
+        classesRepository.findAll().forEach(System.out::println);
+        System.out.println("============");
         classesRepository.deleteAll();
+
+        System.out.println("environment.getProperty(\"container.port\")=" + environment.getProperty("container.port"));
+        LOGGER.info("followOutput = =============================");
     }
 
     @Test
@@ -77,4 +113,19 @@ public class ClassServiceTest {
 
         System.out.println(findClasses.toString());
     }
+
+    @Test
+    void applicationContextInitTest(){
+        // test-container 생성하고 해당 정보를 ApplicationContextInitializer 에 env 를 통해서 등록하기
+
+    }
+
+    static class ContainerPropertyInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext>{
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertyValues.of("container.port=" + genericContainer.getMappedPort(5432))
+                    .applyTo(applicationContext.getEnvironment());
+        }
+    }
+
 }
